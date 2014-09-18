2bd949478f5adf27e03c85e231a913e90ebc693d
Philip Hansen <me@philiphansen.dk>
2014-09-19 00:05:30 +0100

    Display current time

--- timer.js
+++ timer.js
@@ -1,8 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-	var element = document.createElement("p");
-	var text = document.createTextNode("Hi there!");
+var element;
 
+document.addEventListener("DOMContentLoaded", function () {
+	startTimer();
+});
+
+function startTimer() {
+	element = document.createElement("p");
+	updateTime();
+	interval = setInterval(function(){updateTime()}, 1000);
+}
+
+function updateTime() {
+	var text = document.createTextNode(Date());
+
+	while (element.firstChild) {
+		element.removeChild(element.firstChild);
+	}
 	element.appendChild(text);
 
 	document.body.appendChild(element);
-});
+}
