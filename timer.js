870aae487f5f07af01846e37c22e1a4c09408b78
Philip Hansen <me@philiphansen.dk>
2014-09-19 02:13:28 +0100

    Chrome notifications

--- timer.js
+++ timer.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", startTimer);
 chrome.alarms.onAlarm.addListener(onAlarm);
 
 function startTimer() {
+	chrome.notifications.create("", {type: "basic", title: "Stream break!", message: "It's time to take a break from streaming.", iconUrl: "calculator-128.png"}, notificationCallback);
 	element = document.createElement("p");
 	updateTime();
 	chrome.alarms.create("alarm", {periodInMinutes: timeInterval});
@@ -25,5 +26,8 @@ function updateTime() {
 function onAlarm(alarm) {
 	if (alarm && alarm.name == "alarm") {
 		console.log("Alarm!");
+chrome.notifications.create("", {type: "basic", title: "Stream break!", message: "It's time to take a break from streaming.", iconUrl: "calculator-128.png"}, notificationCallback);
 	}
 }
+
+function notificationCallback(notificationId) {}
